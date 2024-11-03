d024aa712a78a49625f44623e8d0b2959dfd48bb
Kyrylo-Smyrnov <kyr.smyrnov@gmail.com>
2024-11-03 03:13:13 +0100

    Usage of EntityHandler in UnitBase & BuildingBase

--- Source/RTSGame/Public/Entities/Units/RGUnitBase.h
+++ Source/RTSGame/Public/Entities/Units/RGUnitBase.h
@@ -4,13 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "Entities/Actions/ActionsList.h"
-#include "Entities/EntitiesImportance.h"
 #include "Entities/Actions/BaseAction.h"
+#include "Entities/EntitiesImportance.h"
 #include "GameFramework/Character.h"
-#include "Player/RGPlayerPawn.h"
 #include "RGUnitBase.generated.h"
 
-class URGMoveToAction;
+class UEntityHandlerComponent;
 class ARGPlayerController;
 class UDecalComponent;
 
@@ -21,32 +20,29 @@ class RTSGAME_API ARGUnitBase : public ACharacter
 
   public:
 	ARGUnitBase();
-
+	
 	bool GetIsSelected() const;
 	int32 GetImportance() const;
 	UTexture2D* GetSelectionIcon() const;
-	TArray<UBaseAction*> GetAvailableActions();
+	TArray<UBaseAction*> GetAvailableActions() const;
 
 	void SetSelected(bool bIsSelected);
 
 	void AddActionToQueue(UBaseAction* Action) const;
+	bool CanPerformAction(UBaseAction* Action) const;
 	void ClearActionQueue() const;
-
-	bool CanPerformAction(UBaseAction* Action);
 
   protected:
 	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION()
 	void HandleOnClicked(AActor* TouchedActor, FKey ButtonPressed);
 	virtual void InitializeActions();
 
-	UPROPERTY()
-	ARGPlayerPawn* PlayerPawn;
-
 	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	UDecalComponent* SelectionCircleDecal;
+	UPROPERTY()
+	UEntityHandlerComponent* EntityHandler;
 
 	UPROPERTY(EditDefaultsOnly, Category = "UI")
 	UTexture2D* SelectionIcon;
